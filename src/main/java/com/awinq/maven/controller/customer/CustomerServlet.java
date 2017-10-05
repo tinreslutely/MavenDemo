bc003546d4f70cc4c8eece17474eafcb4b2f4b5b
XiaoYi <tinreslutely@hotmail.com>
2017-10-05 13:30:48 +0800

    commit
    20171005

--- src/main/java/com/awinq/maven/controller/customer/CustomerServlet.java
+++ src/main/java/com/awinq/maven/controller/customer/CustomerServlet.java
@@ -1,18 +1,31 @@
 package com.awinq.maven.controller.customer;
 
 
+import com.awinq.maven.model.Customer;
+import com.awinq.maven.service.customer.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
+    private CustomerService cutomerService;
+
+    @Override
+    public void init() throws ServletException{
+        cutomerService = new CustomerService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<Customer> customerList = cutomerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
     }
 
     @Override
--- src/main/webapp/WEB-INF/customer/customer.jsp
+++ src/main/webapp/WEB-INF/customer/customer.jsp
@@ -6,11 +6,33 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="BASE" value="${pageContext.request.contextPath}" />
 <html>
 <head>
-    <title>Title</title>
+    <title>客户管理</title>
 </head>
 <body>
+    <h1>客户列表</h1>
+    <table>
+        <tr>客户名称</tr>
+        <tr>联系人</tr>
+        <tr>电话号码</tr>
+        <tr>邮箱地址</tr>
+        <tr>操作</tr>
+    </table>
+    <c:forEach var="customer" items="${customerList}">
+        <tr>
+            <td>${customer.name}</td>
+            <td>${customer.contract}</td>
+            <td>${customer.telephone}</td>
+            <td>${customer.email}</td>
+            <td>
+                <a href="${BASE}/customer_edit?id=${customer.id}">编辑</a>
+                <a href="${BASE}/customer_delete?id=${customer.id}">删除</a>
 
+            </td>
+        </tr>
+    </c:forEach>
 </body>
 </html>
